186af3fe0276edaad8103800dcf02ece9e00bfe4
john_atha <giannisj3@gmail.com>
2020-12-24 21:11:49 +0200

    Add backend skeleton, order docs

---
+++ backend/src/main/java/gr/ntua/ece/softeng35/backend/ServletInitializer.java
@@ -0,0 +1,13 @@
+package gr.ntua.ece.softeng35.backend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BackendApplication.class);
+	}
+
+}
