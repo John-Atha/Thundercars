f482f6be275064b32e5c1999d449b5e16b8263b7
john_atha <giannisj3@gmail.com>
2020-12-29 01:54:54 +0200

    Add some generators for the users' data

---
+++ backend/Scripts/PasswordGenerator.py
@@ -0,0 +1,21 @@
+import random
+import string
+
+letters = list(string.ascii_letters)
+digits = list(string.digits)
+symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_"]
+all = letters * 4 + digits  * 4 + symbols
+
+res = []
+def password(n):
+    global res
+    for i in range(n):
+        temp = ''
+        p1 = random.randrange(30,50)
+        for j in range(p1):
+            if random.randrange(10)>=5:
+                temp = temp + (random.choice(all))
+        res.append(temp)
+    print(res)
+
+password(200)
