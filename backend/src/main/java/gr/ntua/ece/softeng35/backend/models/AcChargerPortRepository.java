fc38baafdc8d5ac090890a2ddee76bd643fa0a26
Thanos Karavangelis <thanosblv@gmail.com>
2020-12-29 15:39:27 +0200

    Comments

--- backend/src/main/java/gr/ntua/ece/softeng35/backend/models/AcChargerPortRepository.java
+++ backend/src/main/java/gr/ntua/ece/softeng35/backend/models/AcChargerPortRepository.java
@@ -1,5 +1,5 @@
-package gr.ntua.ece.softeng35.backend.models;
+//package gr.ntua.ece.softeng35.backend.models;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AcChargerPortRepository extends JpaRepository<AcChargerPort, Integer> {}
+//public interface AcChargerPortRepository extends JpaRepository<AcChargerPort, Integer> {}
