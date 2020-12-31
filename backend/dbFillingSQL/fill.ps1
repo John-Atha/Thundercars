295d5ef2cae2008ff2d9fedc6f00d3b1bf2122e2
Thanos Karavangelis <thanosblv@gmail.com>
2020-12-31 13:46:20 +0200

    Change shell script to run all .sql files in the db.

---
+++ backend/dbFillingSQL/fill.ps1
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#First,you have to run Windows Powershell as administrator and execute the following command:
+#   > Set-ExecutionPolicy RemoteSigned
+
+#Then run in your terminal inside the directory softeng-ntua\backend\dbFillingSQL the command:
+#   > & ./fill.ps1
+
+#If your username is not root you have to change it below after "-u" in every command!
+
+Get-Content ac_charger.sql | mysql -u root db;
+Get-Content dc_charger.sql | mysql -u root db;
+Get-Content countries.sql | mysql -u root db;
+Get-Content addressesall.sql | mysql -u root db;
+Get-Content ac_charger_ports.sql | mysql -u root db;
+Get-Content dc_ports_ids.sql | mysql -u root db;
+Get-Content power_per_charging.sql | mysql -u root; 
+Get-Content vehicles.sql | mysql -u root db;
+Get-Content user_addresses1.sql |mysql -u root db;
+Get-Content users.sql |mysql -u root db;
+Get-Content user_has_vehicle.sql |mysql -u root db;
+Get-Content usage_type.sql |mysql -u root db;
+Get-Content submission_status.sql |mysql -u root db;
+Get-Content status_type.sql |mysql -u root db;
+Get-Content operators.sql |mysql -u root db;
+Get-Content station_owners.sql |mysql -u root db;
+Get-Content current_providers.sql |mysql -u root db;
+Get-Content connection_type.sql |mysql -u root db;
+Get-Content level.sql |mysql -u root db;
+Get-Content current_type.sql |mysql -u root db;
+Get-Content charging_spot.sql |mysql -u root db
+Get-Content stations.sql |mysql -u root db
