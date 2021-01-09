396840fa4a23394966472dbaab39b0f6c1d4bffe
john_atha <giannisj3@gmail.com>
2021-01-09 22:38:18 +0200

    Update ChargingProcess Controller and Repository for 1st api query

--- backend/src/main/java/gr/ntua/ece/softeng35/backend/controllers/ChargingProcessController.java
+++ backend/src/main/java/gr/ntua/ece/softeng35/backend/controllers/ChargingProcessController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.*;
 import gr.ntua.ece.softeng35.backend.models.ChargingProcess;
 import gr.ntua.ece.softeng35.backend.models.ChargingProcessRepository;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.sql.Date;
+
 @RestController
 class ChargingProcessController {
   private final ChargingProcessRepository repository;
@@ -19,6 +24,31 @@ class ChargingProcessController {
   @GetMapping("/evcharge/api/chargingprocesses")
   List<ChargingProcess> all() {
     return repository.findAll();
+  }
+
+  @CrossOrigin(origins = "http://localhost:3000")
+  @GetMapping(value = {"/evcharge/api/SessionsPerStation","/evcharge/api/SessionsPerStation/{stationId}",
+                       "/evcharge/api/SessionsPerStation/{stationId}/{startDate}/{endDate}",
+                       "/evcharge/api/SessionsPerStation/{stationId}/{startDate}",})
+  List<Integer> stationProcess(@PathVariable(required=false) Integer stationId,
+                                       @PathVariable(required=false) String startDate, 
+                                       @PathVariable(required=false) String endDate) {
+    
+    Date date1 = java.sql.Date.valueOf(LocalDate.parse(startDate, DateTimeFormatter.BASIC_ISO_DATE));
+    Date date2 = java.sql.Date.valueOf(LocalDate.parse(endDate, DateTimeFormatter.BASIC_ISO_DATE));
+    
+    /*if (stationId==null) {
+      return repository.findAll();
+    }*/
+    if (stationId!=null && (startDate==null || endDate==null)) {
+      return repository.findByChargingStation_Id(stationId);
+    }
+    else if (stationId!=null && startDate!=null && endDate==null) {
+      return repository.findByChargingStation_IdAndConnectionTimeGreaterThanEqual(stationId, date1);
+    }
+    else {
+      return repository.findByChargingStation_IdAndConnectionTimeGreaterThanEqualAndDisconnectTimeLessThanEqual(stationId, date1, date2);
+    }
   }
 
   @CrossOrigin(origins = "http://localhost:3000")
--- backend/src/main/java/gr/ntua/ece/softeng35/backend/models/ChargingProcessRepository.java
+++ backend/src/main/java/gr/ntua/ece/softeng35/backend/models/ChargingProcessRepository.java
@@ -1,5 +1,32 @@
 package gr.ntua.ece.softeng35.backend.models;
 
+import java.util.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import org.springframework.web.bind.annotation.*;
+
+public interface ChargingProcessRepository extends JpaRepository<ChargingProcess, Integer> {
+
+    /*@Query(value = "SELECT id FROM ChargingProcess u WHERE u.chargingStation_id= ?1", nativeQuery=true)
+    List<Integer> findByChargingStation_Id(Integer chargingStation);
+    List<Integer> findByChargingStation_IdAndConnectionTimeGreaterThanEqualAndDisconnectTimeLessThanEqual(Integer chargingStation, Date connectionTime, Date disconnectTime);
+    List<Integer> findByChargingStation_IdAndConnectionTimeGreaterThanEqual(Integer chargingStation, Date connectionTime);
+    */
     
-public interface ChargingProcessRepository extends JpaRepository<ChargingProcess, Integer> {}
+    @Query(value = "SELECT id FROM ChargingProcess u WHERE u.chargingStation.id = ?1")
+    List<Integer> findByChargingStation_Id(Integer chargingStation);
+
+    @Query(value = "SELECT id FROM ChargingProcess u WHERE u.chargingStation.id = ?1 AND u.connectionTime >= ?2")
+    List<Integer> findByChargingStation_IdAndConnectionTimeGreaterThanEqual(Integer chargingStation, Date connectionTime);
+
+    @Query(value = "SELECT id FROM ChargingProcess u WHERE u.chargingStation.id = ?1 AND u.connectionTime >= ?2 AND u.disconnectTime <= ?3")
+    List<Integer> findByChargingStation_IdAndConnectionTimeGreaterThanEqualAndDisconnectTimeLessThanEqual(Integer chargingStation, Date connectionTime, Date disconnectTime);
+    
+    /*List<Integer> findByStation(Integer chargingStation);
+
+    //@Query(value = "select a from ChargingProcess a where a.chargingStation= :")
+    List<Integer> findByStationAndBothDates(Integer chargingStation, Date connectionTime, Date disconnectTime);
+    List<Integer> findByStationAndStartDate(Integer chargingStation, Date connectionTime);
+    */
+}
