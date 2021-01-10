e53097e927903b8f32eb09bb1cacf3d61f5e3b87
john_atha <giannisj3@gmail.com>
2021-01-10 23:53:18 +0200

    Update 'SessionsPerStation' and 'SessionsPerPoint' endpoints

--- backend/src/main/java/gr/ntua/ece/softeng35/backend/controllers/ChargingProcessController.java
+++ backend/src/main/java/gr/ntua/ece/softeng35/backend/controllers/ChargingProcessController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.*;
 
 import gr.ntua.ece.softeng35.backend.models.ChargingProcess;
 import gr.ntua.ece.softeng35.backend.models.ChargingProcessRepository;
+import gr.ntua.ece.softeng35.backend.models.Operator;
+import gr.ntua.ece.softeng35.backend.models.ChargingStation;
+import gr.ntua.ece.softeng35.backend.models.ChargingStationRepository;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
@@ -15,8 +18,8 @@ import java.math.*;
 
 @RestController
 class ChargingProcessController {
-  private final ChargingProcessRepository repository;
 
+  private final ChargingProcessRepository repository;
   ChargingProcessController(ChargingProcessRepository repository) {
     this.repository = repository;
   }
@@ -27,6 +30,153 @@ class ChargingProcessController {
     return repository.findAll();
   }
 
+
+  /* Returns a list like : [ Station Id, Spot Id, Operator Title, (Browser) Request Time, 
+                             Start Date (given from user), End Date (also from user),
+                             Number of Processes of spot, 
+                             [  [session_index, session_id, connection_time, disconnect_time, program, kWh delivered, payment_way, vehicle_type],
+                                [                 ...                  ],
+                                [                 ...                  ]  ]
+                           ] 
+  */
+
+  @CrossOrigin(origins = "http://localhost:3000")
+  @GetMapping(value = {"/evcharge/api/SessionsPerPoint", "/evcharge/api/SessionsPerPoint/{stationSpotId}",
+                       "/evcharge/api/SessionsPerPoint/{stationSpotId}/{startDate}",
+                       "/evcharge/api/SessionsPerPoint/{stationSpotId}/{startDate}/{endDate}"})
+  List<Object> spotProcess(@PathVariable Optional<Integer> stationSpotId,
+                           @PathVariable Optional<String> startDate,
+                           @PathVariable Optional<String> endDate) {
+
+    if (!stationSpotId.isPresent()) {
+      List<Object> all = new ArrayList();
+
+      for (Integer i=1; i<42979; i++) {
+        Integer stationId =  (Integer) repository.findStationFromTogether(i);
+        if (stationId==null) {
+          List<Object> res = new ArrayList();   // BAD REQUEST
+          all.add(res);
+          continue;
+        }
+        Integer spotIdReal =  (Integer) repository.findSpotFromTogether(i);
+        Integer spotId =  i;
+        List<List<Object>> processes = repository.findBySpotOnly(stationId, spotIdReal);
+        String Operator = (String) repository.findOperator(stationId);
+        Object CurrTime = repository.findTime(stationId);
+        List<Object> res = new ArrayList();
+        Integer counter = 1;
+        for (List<Object> nested : processes) {
+          nested.add(0, counter);
+          counter++;
+        }
+        res.add(stationId);               // station id
+        res.add(spotId);                  // spot id
+        res.add(Operator);                // operator id
+        res.add(CurrTime);                // current time
+        res.add(processes.size());        // number of processes
+        res.add(processes);               // processes list
+        all.add(res);
+      }
+      
+      return all;
+    }
+
+    else if (stationSpotId.isPresent() && !startDate.isPresent() && !endDate.isPresent()) {
+      Integer stationId =  (Integer) repository.findStationFromTogether(stationSpotId.get());
+      if (stationId==null) {
+        List<Object> res = new ArrayList();   // BAD REQUEST
+        return res;
+      }
+      Integer spotIdReal =  (Integer) repository.findSpotFromTogether(stationSpotId.get());
+      Integer spotId =  stationSpotId.get();
+      List<List<Object>> processes = repository.findBySpotOnly(stationId, spotIdReal);
+      String Operator = (String) repository.findOperator(stationId);
+      Object CurrTime = repository.findTime(stationId);
+      List<Object> res = new ArrayList();
+      Integer counter = 1;
+      for (List<Object> nested : processes) {
+        nested.add(0, counter);
+        counter++;
+      }
+      res.add(stationId);               // station id
+      res.add(spotId);                  // spot id
+      res.add(Operator);                // operator id
+      res.add(CurrTime);                // current time
+      res.add(processes.size());        // number of processes
+      res.add(processes);               // processes list
+      return res;
+    }
+
+    else if (stationSpotId.isPresent() && startDate.isPresent() && !endDate.isPresent()) {
+      Date date1 = java.sql.Date.valueOf(LocalDate.parse(startDate.get(), DateTimeFormatter.BASIC_ISO_DATE));
+      Integer stationId =  (Integer) repository.findStationFromTogether(stationSpotId.get());
+      if (stationId==null) {
+        List<Object> res = new ArrayList();   // BAD REQUEST
+        return res;
+      }
+      Integer spotIdReal =  (Integer) repository.findSpotFromTogether(stationSpotId.get());
+      Integer spotId =  stationSpotId.get();
+      List<List<Object>> processes = repository.findBySpotAndStartDate(stationId, spotIdReal, date1);
+      String Operator = (String) repository.findOperator(stationId);
+      Object CurrTime = repository.findTime(stationId);
+      List<Object> res = new ArrayList();
+      Integer counter = 1;
+      for (List<Object> nested : processes) {
+        nested.add(0, counter);
+        counter++;
+      }
+      res.add(stationId);               // station id
+      res.add(spotId);                  // spot id
+      res.add(Operator);                // operator id
+      res.add(CurrTime);                // current time
+      res.add(date1.toString());        // start date
+      res.add(processes.size());        // number of processes
+      res.add(processes);               // processes list
+      return res;
+    }
+
+    else {
+      Date date1 = java.sql.Date.valueOf(LocalDate.parse(startDate.get(), DateTimeFormatter.BASIC_ISO_DATE));
+      Date date2 = java.sql.Date.valueOf(LocalDate.parse(endDate.get(), DateTimeFormatter.BASIC_ISO_DATE));
+      Integer stationId =  (Integer) repository.findStationFromTogether(stationSpotId.get());
+      if (stationId==null) {
+        List<Object> res = new ArrayList();   // BAD REQUEST
+        return res;
+      }
+      Integer spotIdReal =  (Integer) repository.findSpotFromTogether(stationSpotId.get());
+      Integer spotId =  stationSpotId.get();
+      List<List<Object>> processes = repository.findBySpotAndBothDates(stationId, spotIdReal, date1, date2);
+      String Operator = (String) repository.findOperator(stationId);
+      Object CurrTime = repository.findTime(stationId);
+      List<Object> res = new ArrayList();
+      Integer counter = 1;
+      for (List<Object> nested : processes) {
+        nested.add(0, counter);
+        counter++;
+      }
+      res.add(stationId);               // station id
+      res.add(spotId);                  // spot id
+      res.add(Operator);                // operator id
+      res.add(CurrTime);                // current time
+      res.add(date1.toString());        // start date
+      res.add(processes.size());        // number of processes
+      res.add(processes);               // processes list
+      return res;
+    }  
+  
+  }
+  
+
+  /* Returns a list like : [ Station Id, Operator Title, (Browser) Request Time, 
+                             Start Date (given from user), End Date (also from user),
+                             Total kWh Dekivered from the Station, Total Processes,
+                             Number of Distinct Spots used, 
+                             [  [spot_id, processes here, kWh delivered],
+                                [                 ...                  ],
+                                [                 ...                  ]  ]
+                           ] 
+  */
+
   @CrossOrigin(origins = "http://localhost:3000")
   @GetMapping(value = {"/evcharge/api/SessionsPerStation","/evcharge/api/SessionsPerStation/{stationId}",
                        "/evcharge/api/SessionsPerStation/{stationId}/{startDate}/{endDate}",
@@ -34,11 +184,95 @@ class ChargingProcessController {
   List<Object> stationProcess(@PathVariable Optional<Integer> stationId,
                                        @PathVariable Optional<String> startDate, 
                                        @PathVariable Optional<String> endDate) {
-    
-    if (stationId.isPresent() && !startDate.isPresent() && !endDate.isPresent()) {
+
+    if (!stationId.isPresent()) {
+
+      List<Object> all = new ArrayList();
+
+      for (Integer i=1; i<7663; i++) {
+        
+        List<List<Object>> temp = repository.findByStation(i);
+        List<Object> res = new ArrayList();
+        Boolean flag = false;
+        if (temp.get(0).get(0)==null) {
+          flag = true;
+          temp = repository.findByStation2(i);
+          if (temp.size()==0) {
+            all.add(res);           // WRONG STATION ID
+            continue;
+          }
+          temp.get(0).add(3, 0);
+          temp.get(0).add(3, 0);
+          temp.get(0).add(3, 0);
+        }
+        /* retrieve sessions groupped by station-spot */
+        List<List<Object>> PointsSessions = repository.findByStationGroupPoints(i);
+        
+        if (temp.size()>0) {
+          Double totalkWh = 0.0;
+          Long sessions = 0L;
+          
+          /* total Station kWh compute */
+          for (List<Object> nested : PointsSessions) {
+            totalkWh = totalkWh + (Double) nested.get(2);
+          }
+          /* total Station Sessions compute */
+          for (List<Object> nested : PointsSessions) {
+            sessions = sessions + (Long) nested.get(1);
+          }
+
+          /*for (List<Object> nested : PointsSessions) {
+            String spotid = nested.get(0).toString();
+            String stationid = stationId.get().toString();
+            nested.remove(0);
+            String stationspotid = stationid+"-"+spotid;
+            nested.add(0, stationspotid);
+          }
+          */
+
+          for (List<Object> nested : PointsSessions) {
+            Integer spotid = (Integer) nested.get(0);
+            Integer stationid = (Integer) i;
+            nested.remove(0);
+            Integer stationspotid = repository.findStationSpotId(stationid, spotid);
+            nested.add(0, stationspotid);
+          }
+
+          if (flag==true) {
+            res.add(temp.get(0).get(0));  // station id
+            res.add(temp.get(0).get(1));  // operator title
+            res.add(temp.get(0).get(2));  // current time
+          }
+          else {
+            res.add(temp.get(0).get(1));  // station id
+            res.add(temp.get(0).get(2));  // operator title
+            res.add(temp.get(0).get(3));  // current time
+          }
+          res.add(totalkWh);            // total kWh
+          res.add(sessions);         // sessions counter
+          res.add(temp.get(0).get(5));  // number of distinct spots used
+          res.add(PointsSessions);  
+        } 
+        
+        all.add(res);
+      }
+      return all;
+    }                                    
+
+    else if (stationId.isPresent() && !startDate.isPresent() && !endDate.isPresent()) {
       List<List<Object>> temp = repository.findByStation(stationId.get());
       List<Object> res = new ArrayList();
-      
+      Boolean flag = false;
+      if (temp.get(0).get(0)==null) {
+        flag = true;
+        temp = repository.findByStation2(stationId.get());
+        if (temp.size()==0) {
+          return res;           // WRONG STATION ID
+        }
+        temp.get(0).add(3, 0);
+        temp.get(0).add(3, 0);
+        temp.get(0).add(3, 0);
+      }
       /* retrieve sessions groupped by station-spot */
       List<List<Object>> PointsSessions = repository.findByStationGroupPoints(stationId.get());
       
@@ -55,9 +289,33 @@ class ChargingProcessController {
           sessions = sessions + (Long) nested.get(1);
         }
 
-        res.add(temp.get(0).get(1));  // station id
-        res.add(temp.get(0).get(2));  // operator title
-        res.add(temp.get(0).get(3));  // current time
+        /*for (List<Object> nested : PointsSessions) {
+          String spotid = nested.get(0).toString();
+          String stationid = stationId.get().toString();
+          nested.remove(0);
+          String stationspotid = stationid+"-"+spotid;
+          nested.add(0, stationspotid);
+        }
+        */
+
+        for (List<Object> nested : PointsSessions) {
+          Integer spotid = (Integer) nested.get(0);
+          Integer stationid = (Integer) stationId.get();
+          nested.remove(0);
+          Integer stationspotid = repository.findStationSpotId(stationid, spotid);
+          nested.add(0, stationspotid);
+        }
+
+        if (flag==true) {
+          res.add(temp.get(0).get(0));  // station id
+          res.add(temp.get(0).get(1));  // operator title
+          res.add(temp.get(0).get(2));  // current time
+        }
+        else {
+          res.add(temp.get(0).get(1));  // station id
+          res.add(temp.get(0).get(2));  // operator title
+          res.add(temp.get(0).get(3));  // current time
+        }
         res.add(totalkWh);            // total kWh
         res.add(sessions);         // sessions counter
         res.add(temp.get(0).get(5));  // number of distinct spots used
@@ -65,12 +323,23 @@ class ChargingProcessController {
       }    
       return res;
     }
+
     else if (stationId.isPresent() && startDate.isPresent() && !endDate.isPresent()) {
 
       Date date1 = java.sql.Date.valueOf(LocalDate.parse(startDate.get(), DateTimeFormatter.BASIC_ISO_DATE));
       List<List<Object>> temp = repository.findByStationAndStartDate(stationId.get(), date1);
       List<Object> res = new ArrayList();
-      
+      Boolean flag = false;
+      if (temp.get(0).get(0)==null) {
+        flag = true;
+        temp = repository.findByStation2(stationId.get());
+        if (temp.size()==0) {
+          return res;           // WRONG STATION ID
+        }
+        temp.get(0).add(3, 0);
+        temp.get(0).add(3, 0);
+        temp.get(0).add(3, 0);
+      }
       /* retrieve sessions groupped by station-spot */
       List<List<Object>> PointsSessions = repository.findByStationStartDateGroupPoints(stationId.get(), date1);
 
@@ -86,10 +355,34 @@ class ChargingProcessController {
         for (List<Object> nested : PointsSessions) {
           sessions = sessions + (Long) nested.get(1);
         }
-        
-        res.add(temp.get(0).get(1));  // station id
-        res.add(temp.get(0).get(2));  // operator title
-        res.add(temp.get(0).get(3));  // current time
+
+        /*for (List<Object> nested : PointsSessions) {
+          String spotid = nested.get(0).toString();
+          String stationid = stationId.get().toString();
+          nested.remove(0);
+          String stationspotid = stationid+"-"+spotid;
+          nested.add(0, stationspotid);
+        }
+        */
+
+        for (List<Object> nested : PointsSessions) {
+          Integer spotid = (Integer) nested.get(0);
+          Integer stationid = (Integer) stationId.get();
+          nested.remove(0);
+          Integer stationspotid = repository.findStationSpotId(stationid, spotid);
+          nested.add(0, stationspotid);
+        }
+
+        if (flag==true) {
+          res.add(temp.get(0).get(0));  // station id
+          res.add(temp.get(0).get(1));  // operator title
+          res.add(temp.get(0).get(2));  // current time
+        }
+        else {
+          res.add(temp.get(0).get(1));  // station id
+          res.add(temp.get(0).get(2));  // operator title
+          res.add(temp.get(0).get(3));  // current time
+        }
         res.add(date1.toString());    // start date
         res.add(totalkWh);            // total kWh
         res.add(sessions);         // sessions counter
@@ -98,12 +391,23 @@ class ChargingProcessController {
       }
       return res;
     }
+
     else {
       Date date1 = java.sql.Date.valueOf(LocalDate.parse(startDate.get(), DateTimeFormatter.BASIC_ISO_DATE));
       Date date2 = java.sql.Date.valueOf(LocalDate.parse(endDate.get(), DateTimeFormatter.BASIC_ISO_DATE));
-      
       List<List<Object>> temp = repository.findByStationAndTimestamp(stationId.get(), date1, date2);
       List<Object> res = new ArrayList();
+      Boolean flag = false;
+      if (temp.get(0).get(0)==null) {
+        flag = true;
+        temp = repository.findByStation2(stationId.get());
+        if (temp.size()==0) {
+          return res;           // WRONG STATION ID
+        }
+        temp.get(0).add(3, 0);
+        temp.get(0).add(3, 0);
+        temp.get(0).add(3, 0);
+      }
       
       /* retrieve sessions groupped by station-spot */
       List<List<Object>> PointsSessions = repository.findByStationBothDatesGroupPoints(stationId.get(), date1, date2);
@@ -121,9 +425,35 @@ class ChargingProcessController {
           sessions = sessions + (Long) nested.get(1);
         }
 
-        res.add(temp.get(0).get(1));  // station id
-        res.add(temp.get(0).get(2));  // operator title
-        res.add(temp.get(0).get(3));  // current time
+        /*
+        for (List<Object> nested : PointsSessions) {
+          String spotid = nested.get(0).toString();
+          String stationid = stationId.get().toString();
+          nested.remove(0);
+          String stationspotid = stationid+"-"+spotid;
+          nested.add(0, stationspotid);
+        }
+        */
+
+        for (List<Object> nested : PointsSessions) {
+          Integer spotid = (Integer) nested.get(0);
+          Integer stationid = (Integer) stationId.get();
+          nested.remove(0);
+          Integer stationspotid = repository.findStationSpotId(stationid, spotid);
+          nested.add(0, stationspotid);
+        }
+
+        //res.add(temp);
+        if (flag==true) {
+          res.add(temp.get(0).get(0));  // station id
+          res.add(temp.get(0).get(1));  // operator title
+          res.add(temp.get(0).get(2));  // current time
+        }
+        else {
+          res.add(temp.get(0).get(1));  // station id
+          res.add(temp.get(0).get(2));  // operator title
+          res.add(temp.get(0).get(3));  // current time
+        }
         res.add(date1.toString());    // start date
         res.add(date2.toString());    // end date
         res.add(totalkWh);            // total kWh
--- backend/src/main/java/gr/ntua/ece/softeng35/backend/models/ChargingProcessRepository.java
+++ backend/src/main/java/gr/ntua/ece/softeng35/backend/models/ChargingProcessRepository.java
@@ -11,6 +11,9 @@ public interface ChargingProcessRepository extends JpaRepository<ChargingProcess
     @Query(value = "SELECT u.id, u.chargingStation.id, u.chargingStation.operator.title, CURRENT_TIME, u.kwhDelivered, COUNT( DISTINCT u.chargingSpot.id), u.chargingSpot.id FROM ChargingProcess u WHERE u.chargingStation.id = ?1 ORDER BY u.chargingSpot.id")
     List<List<Object>> findByStation(Integer chargingStation);
 
+    @Query(value = "SELECT u.id, u.operator.title, CURRENT_TIME FROM ChargingStation u WHERE u.id = ?1")
+    List<List<Object>> findByStation2(Integer chargingStation);
+
     @Query(value = "SELECT u.chargingSpot.id, COUNT(*), SUM(u.kwhDelivered) FROM ChargingProcess u WHERE u.chargingStation.id = ?1 GROUP BY u.chargingSpot.id")
     List<List<Object>> findByStationGroupPoints(Integer chargingStation);
 //--------------------------------------    
@@ -27,4 +30,37 @@ public interface ChargingProcessRepository extends JpaRepository<ChargingProcess
 
     @Query(value = "SELECT u.chargingSpot.id, COUNT(*), SUM(u.kwhDelivered) FROM ChargingProcess u WHERE u.chargingStation.id = ?1 AND u.connectionTime >= ?2 AND u.disconnectTime <= ?3 GROUP BY u.chargingSpot.id")
     List<List<Object>> findByStationBothDatesGroupPoints(Integer chargingStation, Date connectionTime, Date disconnectTime);
+//---------------------------------------
+//---------------------------------------
+    @Query(value = "SELECT u.id FROM ChargingStationSpots u WHERE u.chargingStation.id = ?1 AND u.chargingSpot.id = ?2")
+    Integer findStationSpotId(Integer chargingStation, Integer ChargingSpot);
+
+    @Query(value = "SELECT u.chargingStation.id FROM ChargingStationSpots u WHERE u.id = ?1")
+    Integer findStationFromTogether(Integer chargingStationSpots);
+
+    @Query(value = "SELECT u.chargingSpot.id FROM ChargingStationSpots u WHERE u.id = ?1")
+    Integer findSpotFromTogether(Integer chargingStationSpots);
+
+
+    @Query(value = "SELECT u.operator.title FROM ChargingStation u WHERE u.id= ?1")
+    String findOperator(Integer stationId);
+    
+    @Query(value = "SELECT CURRENT_TIME FROM ChargingStation u WHERE u.id= ?1")
+    Object findTime(Integer stationId);
+
+//---------------------------------------
+//---------------------------------------
+   // @Query(value = "SELECT u.id, u.chargingStation.id, u.chargingSpot.id, u.chargingStation.operator.title, CURRENT_TIME, COUNT( DISTINCT u.chargingSpot.id) FROM ChargingProcess u WHERE u.chargingStation.id = ?1 ORDER BY u.chargingSpot.id")
+   // List<List<Object>> findBySpot(Integer chargingSpot);
+
+    @Query( value = "SELECT u.id, u.connectionTime, u.doneChargingTime, u.disconnectTime, u.chargingProgram, u.kwhDelivered, u.paymentWay, u.vehicle.type FROM ChargingProcess u WHERE u.chargingStation.id= ?1 AND u.chargingSpot.id= ?2")
+    List<List<Object>> findBySpotOnly(Integer chargingStation, Integer chargingSpot);
+
+    @Query( value = "SELECT u.id, u.connectionTime, u.doneChargingTime, u.disconnectTime, u.chargingProgram, u.kwhDelivered, u.paymentWay, u.vehicle.type FROM ChargingProcess u WHERE u.chargingStation.id= ?1 AND u.chargingSpot.id= ?2 AND u.connectionTime >= ?3")
+    List<List<Object>> findBySpotAndStartDate(Integer chargingStation, Integer chargingSpot, Date startDate);
+
+    @Query( value = "SELECT u.id, u.connectionTime, u.doneChargingTime, u.disconnectTime, u.chargingProgram, u.kwhDelivered, u.paymentWay, u.vehicle.type FROM ChargingProcess u WHERE u.chargingStation.id= ?1 AND u.chargingSpot.id= ?2 AND u.connectionTime >= ?3 AND u.disconnectTime <= ?4")
+    List<List<Object>> findBySpotAndBothDates(Integer chargingStation, Integer chargingSpot, Date startDate, Date endDate);
+
+
 }
