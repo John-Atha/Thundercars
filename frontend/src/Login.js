317cf96dbbb52c5da73470046872e015dd3abf6e
john_atha <giannisj3@gmail.com>
2021-02-19 21:02:36 +0200

    Update sumbit-button deactivation

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -15,6 +15,8 @@ class Login extends React.Component {
         this.handleInput = this.handleInput.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);    
         this.submitActivate = this.submitActivate.bind(this);    
+        this.submitChangeColor = this.submitChangeColor.bind(this);    
+  
     }
 
     handleInput = (e) => {
@@ -33,24 +35,38 @@ class Login extends React.Component {
         e.preventDefault();
     }
 
-    submitActivate = () => {
-        if (this.state.username===null || this.state.password===null) {
-            this.state.sumbitDisabled = true
-            console.log("submit deactivated");
+    submitChangeColor = () => {
+        const submitButton = document.getElementById('submit-input');
+        if (this.state.sumbitDisabled) {
+            submitButton.style.backgroundColor="#9999";
+            submitButton.style.color="white";
         }
         else {
-            if (this.state.username.length===0 || this.state.password.length===0) {
-                this.state.sumbitDisabled = true
-                console.log("submit deactivated");
-            }
-            else {
-                this.state.sumbitDisabled = false
-                console.log("submit activated");
-            }
+            submitButton.style.backgroundColor="rgb(233, 21, 21)";
+            submitButton.style.color="white";
         }
     }
 
-
+    submitActivate = () => {
+        if (this.state.username===null || this.state.password===null) {
+            console.log("submit deactivated1");
+            this.state.sumbitDisabled=true;
+        }
+        else {
+            console.log(this.state.username.length);
+            console.log(this.state.password.length);
+            if (this.state.username.length===0 || this.state.password.length===0) {
+                console.log("submit deactivated1");
+                this.state.sumbitDisabled=true;
+            }
+            else {
+                console.log("submit activated1");
+                this.state.sumbitDisabled=false;
+            }
+        }
+        this.submitChangeColor();
+    }
+ 
     render() {
         return (
             <div className="login-page-container blur center-content">
@@ -65,14 +81,14 @@ class Login extends React.Component {
                         Login
                     </div>
                     <form id="login-form">
-                        <input id="username-input" className="login-input" name="username" value={this.state.username} type="text" placeholder="Username..." onChange={this.handleInput} onKeyUp ={this.submitActivate}/>
-                        <input id="password-input" className="login-input" name="password" value={this.state.password} type="password" placeholder="Password..." onChange={this.handleInput} onKeyUp ={this.submitActivate}/>
-                        <input id="submit-input" className="login-input" name="submit" type="submit" value="Submit" disabled={this.state.sumbitDisabled} onClick={this.handleSumbit}/>
+                        <input id="username-input" className="login-input" name="username" value={this.state.username} type="text" placeholder="Username..." onChange={this.handleInput} onKeyUp ={this.submitActivate} />
+                        <input id="password-input" className="login-input" name="password" value={this.state.password} type="password" placeholder="Password..." onChange={this.handleInput} onKeyUp ={this.submitActivate} />
+                        <input id="submit-input" className="login-input" name="submit" type="submit" value="Submit" disabled={this.state.sumbitDisabled} onClick={this.handleSubmit}/>
                     </form>
                 </div>
                 <div className="register-container">
                     First time here?<br></br>
-                    <a className="register-link" href="#">Create an account.</a>            
+                    <a className="register-link" href="/register">Create an account.</a>            
                 </div>  
             </div>
         )
