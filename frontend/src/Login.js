742200052f1165e2bae66c8be6699346ace7f3a3
john_atha <giannisj3@gmail.com>
2021-02-19 17:31:12 +0200

    Add event listeners to login input fields

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -7,11 +7,49 @@ class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            username: null,
-            password: null,
+            username: "",
+            password: "",
+            sumbitDisabled: true,
             error: null
-        }    
+        }
+        this.handleInput = this.handleInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);    
+        this.submitActivate = this.submitActivate.bind(this);    
     }
+
+    handleInput = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState( {
+            [name]: value
+        })
+    }
+
+    handleSubmit = (e) => {
+        this.setState( {
+            error: null
+        });
+        // to be continued...
+        e.preventDefault();
+    }
+
+    submitActivate = () => {
+        if (this.state.username===null || this.state.password===null) {
+            this.state.sumbitDisabled = true
+            console.log("submit deactivated");
+        }
+        else {
+            if (this.state.username.length===0 || this.state.password.length===0) {
+                this.state.sumbitDisabled = true
+                console.log("submit deactivated");
+            }
+            else {
+                this.state.sumbitDisabled = false
+                console.log("submit activated");
+            }
+        }
+    }
+
 
     render() {
         return (
@@ -27,9 +65,9 @@ class Login extends React.Component {
                         Login
                     </div>
                     <form id="login-form">
-                        <input id="username-input" className="login-input"name="username" type="text" placeholder="Username..."/>
-                        <input id="password-input" className="login-input" name="password" type="text" placeholder="Password..."/>
-                        <input id="submit-input" className="login-input" name="submit" type="submit" value="Submit"/>
+                        <input id="username-input" className="login-input" name="username" value={this.state.username} type="text" placeholder="Username..." onChange={this.handleInput} onKeyUp ={this.submitActivate}/>
+                        <input id="password-input" className="login-input" name="password" value={this.state.password} type="password" placeholder="Password..." onChange={this.handleInput} onKeyUp ={this.submitActivate}/>
+                        <input id="submit-input" className="login-input" name="submit" type="submit" value="Submit" disabled={this.state.sumbitDisabled} onClick={this.handleSumbit}/>
                     </form>
                 </div>
                 <div className="register-container">
@@ -37,7 +75,6 @@ class Login extends React.Component {
                     <a className="register-link" href="#">Create an account.</a>            
                 </div>  
             </div>
-
         )
     }
 
