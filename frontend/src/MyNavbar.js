335ba528c9b06c461369b05a402f3d76d4dd320a
john_atha <giannisj3@gmail.com>
2021-02-23 10:50:29 +0200

    Update navbar links to be shown conditionally

--- frontend/src/MyNavbar.js
+++ frontend/src/MyNavbar.js
@@ -7,19 +7,34 @@ import Nav from 'react-bootstrap/Nav';
 class MyNavbar extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      userId: localStorage.getItem('userId'),
+      role: localStorage.getItem('role')
+    }
   }
   
   render() {
     return (
       <Navbar className="color-nav">
         <Nav className="mr-auto">
-          <Nav.Link id="nav-link" href="#">Home</Nav.Link>
-          <Nav.Link id="nav-link" href="#">My vehicles</Nav.Link>
-          <Nav.Link id="nav-link" href="#">My statistics</Nav.Link>
-          <Nav.Link id="nav-link" href="#">My stations</Nav.Link>
+          <Nav.Link id="nav-link" href="/home">Home</Nav.Link>
+          {this.state.role==="VehicleOwner" && (
+            <Nav.Link id="nav-link" href="/myVehicles">My vehicles</Nav.Link>
+          )}
+          <Nav.Link id="nav-link" href="/myStatistics">My statistics</Nav.Link>
+          {this.state.role=="StationOwner" && (
+            <Nav.Link id="nav-link" href="#">My stations</Nav.Link>
+          )}
           <Nav.Link id="nav-link" href="#">Detailed Sessions</Nav.Link>
-          <Nav.Link id="nav-link" href="#">Login</Nav.Link>
-          <Nav.Link id="nav-link" href="#">Logout</Nav.Link>
+          {!this.state.userId && (
+            <Nav.Link id="nav-link" href="/">Login</Nav.Link>
+          )}
+          {!this.state.userId && (
+            <Nav.Link id="nav-link" href="/register">Register</Nav.Link>
+          )}
+          {this.state.userId && (
+            <Nav.Link id="nav-link" href="#">Logout</Nav.Link>
+          )}
         </Nav>
       </Navbar>
       
