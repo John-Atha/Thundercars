3420c0889f0a3bee20ac457e642c8f0ad51424fe
john_atha <giannisj3@gmail.com>
2021-02-28 19:21:26 +0200

    Add dummy api key for front end

--- backend/dbFillingSQL/admins.sql
+++ backend/dbFillingSQL/admins.sql
@@ -1,4 +1,4 @@
-INSERT INTO `db`.`admin`(`date_of_birth`, `email_addr`, `first_name`, `last_name`, `password`, `username`) VALUES("1999-07-12", "headadmin@windowslive.com", "Admin", "admin", "6253ada809602723f9e4f53092443820", "admin");
+INSERT INTO `db`.`admin`(`date_of_birth`, `email_addr`, `first_name`, `last_name`, `password`, `username`, `api_key`) VALUES("1999-07-12", "headadmin@windowslive.com", "Admin", "admin", "6253ada809602723f9e4f53092443820", "admin", "123456789");
 INSERT INTO `db`.`admin`(`date_of_birth`, `email_addr`, `first_name`, `last_name`, `password`, `username`) VALUES("1998-06-11", "admin2@windowslive.com", "Andreas", "Mantz", "e024f9589c1e9d64b34cb1257d9c9dfc", "andreas");
 INSERT INTO `db`.`admin`(`date_of_birth`, `email_addr`, `first_name`, `last_name`, `password`, `username`) VALUES("1997-05-10", "admin3@mail.com", "Antonis", "Tsi", "1351cd4c983681e73e4c9a8348c7e0d9", "antonis");
 INSERT INTO `db`.`admin`(`date_of_birth`, `email_addr`, `first_name`, `last_name`, `password`, `username`) VALUES("1996-04-09", "admin4@windowslive.com", "Thanos", "Kara", "2c6c2b29fa7a56495478e7198ce1d1ef", "thanos");
