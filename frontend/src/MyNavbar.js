bcaca126a92548454bcb9f05af2f520a1c8a84f0
john_atha <giannisj3@gmail.com>
2021-03-13 15:21:25 +0200

    Update navbar links to be shown after checking token

--- frontend/src/MyNavbar.js
+++ frontend/src/MyNavbar.js
@@ -4,15 +4,17 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import icon from './images/user-icon.png';
-import {logoutPost} from './api';
+import {logoutPost, isLogged} from './api';
 
 class MyNavbar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       userId: localStorage.getItem('userId'),
-      role: localStorage.getItem('role')
+      role: localStorage.getItem('role'),
+      logged: false,
     }
+    this.logout = this.logout.bind(this);
   }
 
   logout = () => {
@@ -34,9 +36,22 @@ class MyNavbar extends React.Component {
     })
   }
 
+  componentDidMount() {
+    isLogged()
+        .then(response => {
+            console.log(response);
+            this.setState({
+                logged: true,
+            })
+        })
+        .catch(err => {
+          console.log(err);
+          this.setState({
+            logged: false,
+          })
+        })
+  }
 
-
-  //      <Nav.Link id="nav-link" href="/myStatistics">My statisdgsstics</Nav.Link>
 
   render() {
     return (
@@ -44,27 +59,27 @@ class MyNavbar extends React.Component {
         <Nav className="mr-auto">
           <Nav.Link id="nav-link" href="/">Home</Nav.Link>
 
-          {this.state.role==="StationOwner" && (
+          { this.state.logged && this.state.role==="StationOwner" && (
           <NavDropdown title="My statistics" id="nav-link">
             <NavDropdown.Item href="/myStationStatistics">Sessions per Station</NavDropdown.Item>
             <NavDropdown.Item href="/StationsMonthlyStatistics">Monthly</NavDropdown.Item>
           </NavDropdown>
           )}
-          {this.state.role==="VehicleOwner" && (
+          { this.state.logged && this.state.role==="VehicleOwner" && (
           <NavDropdown title="My statistics" id="nav-link">
             <NavDropdown.Item href="/UserMyStatistics">General</NavDropdown.Item>
             <NavDropdown.Item href="/UserVehicleStatistics">Per vehicle</NavDropdown.Item>
           </NavDropdown>
           )}
 
-          {this.state.role==="StationOwner" && (
+          { this.state.logged && this.state.role==="StationOwner" && (
             <NavDropdown title="Stations" id="nav-link">
               <NavDropdown.Item href="/myStations">My stations</NavDropdown.Item>
               <NavDropdown.Item href="/addStation">Add new station</NavDropdown.Item>
             </NavDropdown>
           )}
 
-          {this.state.role==="StationOwner" && (
+          {this.state.logged && this.state.role==="StationOwner" && (
             <NavDropdown title="Spots" id="nav-link">
               <NavDropdown.Item href="/mySpots">My spots</NavDropdown.Item>
               <NavDropdown.Item href="/addSpot">Add new spot</NavDropdown.Item>
@@ -72,30 +87,27 @@ class MyNavbar extends React.Component {
           )}
 
 
-          {this.state.role==="VehicleOwner" && (
+          {this.state.logged && this.state.role==="VehicleOwner" && (
               <NavDropdown title="Vehicles" id="nav-link">
                 <NavDropdown.Item href="/myVehicles">My vehicles</NavDropdown.Item>
                 <NavDropdown.Item href="/addVehicle">Add vehicle</NavDropdown.Item>
               </NavDropdown>
           )}
 
-          {this.state.role==="StationOwner" && (
+          {this.state.logged && this.state.role==="StationOwner" && (
             <Nav.Link id="nav-link" href="/MySpotsDetailedSessions">Detailed Sessions</Nav.Link>
           )}
 
-          {this.state.role==="VehicleOwner" && (
+          {this.state.logged && this.state.role==="VehicleOwner" && (
             <Nav.Link id="nav-link" href="/VehiclesDetailedSessions">Detailed Sessions</Nav.Link>
           )}
-          {this.state.userId && (
-            <Nav.Link id="nav-link" href="/developers">For developers</Nav.Link>
-          )}
-          {!this.state.userId && (
+          {!this.state.logged && (
             <Nav.Link id="nav-link" href="/login">Login</Nav.Link>
           )}
-          {!this.state.userId && (
+          {!this.state.logged && (
             <Nav.Link id="nav-link" href="/register">Register</Nav.Link>
           )}
-          {this.state.userId && (
+          {this.state.logged && (
             <NavDropdown title={<img className="user-icon user-choices" src={icon} alt="user icon"/>} id="nav-link">
               <NavDropdown.Item className="user-choices2" href="/profile">My profile</NavDropdown.Item>
               <NavDropdown.Item className="user-choices" onClick={this.logout}>Logout</NavDropdown.Item>
