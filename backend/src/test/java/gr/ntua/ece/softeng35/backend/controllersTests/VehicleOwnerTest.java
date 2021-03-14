1410ea0ccb9a8a5d4da52a1f0d80c01dcead1149
Thanos Karavangelis <thanosblv@gmail.com>
2021-03-14 22:47:24 +0200

    Fix Vehicle Owner unit tests

--- backend/src/test/java/gr/ntua/ece/softeng35/backend/controllersTests/VehicleOwnerTest.java
+++ backend/src/test/java/gr/ntua/ece/softeng35/backend/controllersTests/VehicleOwnerTest.java
@@ -43,7 +43,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.sql.Date;
 
-@WebMvcTest(UserHasVehicleController.class)
+@WebMvcTest(UserController.class)
 class VehicleOwnerTest {
 
     @Autowired
@@ -53,10 +53,13 @@ class VehicleOwnerTest {
     WebApplicationContext wac;
 
     @MockBean
-    private UserHasVehicleRepository repository;
+    private UserRepository repository2;
 
     @MockBean
-    private UserRepository repository2;
+    private StationOwnerRepository repository4;
+
+    @MockBean
+    private AdminRepository repository5;
     
     @MockBean
     private AcChargerRepository repository11;
@@ -78,6 +81,8 @@ class VehicleOwnerTest {
 
         MockitoAnnotations.initMocks(this);
         this.mockmvc = webAppContextSetup(this.wac).build();
+
+        Date date = java.sql.Date.valueOf(LocalDate.parse("20201010", DateTimeFormatter.BASIC_ISO_DATE));
         
         Country testCountry = new Country(1,"mytitle","myiso", "mycont");
         
@@ -87,42 +92,45 @@ class VehicleOwnerTest {
         User testUser = new User("user1", "pass1", "email1" , "name1", "name2" , date, testUserAddress,"apikey" );
 
         List<User> myList = Arrays.asList(testUser);
+        BDDMockito.when(repository2.findAll()).thenReturn(myList);
 
-        BDDMockito.when(repository.findAll()).thenReturn(myLiist);
+        List<User> users = new ArrayList();
+        List<Admin> admins = new ArrayList();
+        List<StationOwner> stationOwners = new ArrayList();
+        BDDMockito.when(repository2.findByIdAndApiKey(1,"1")).thenReturn(users);
+        BDDMockito.when(repository4.findByIdAndApiKey(1,"1")).thenReturn(stationOwners);
+        BDDMockito.when(repository5.findByIdAndApiKey(1,"1")).thenReturn(admins);
 
-        List<Integer> apiKeys = new ArrayList<>();
-        apiKeys.add(1);
-
-        BDDMockito.when(repository2.findAdminByApiKey("123456789")).thenReturn(apiKeys);
-
-        this.mockmvc.perform(get("/evcharge/api/{apikey}/users","123456789")
-            .contentType(MediaType.APPLICATION_JSON))
+        this.mockmvc.perform(get("/evcharge/api/users")
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("X-OBSERVATORY-AUTH", "wiefweifhbv2397f2vfu22837514899tyjiwbc"))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$[0].user.id", is(testUser.getId())))
-            .andExpect(jsonPath("$[0].user.username", is(testUser.getUsername())))
-            .andExpect(jsonPath("$[0].user.password", is(testUser.getPassword())))
-            .andExpect(jsonPath("$[0].user.emailAddr", is(testUser.getEmailAddr())))
-            .andExpect(jsonPath("$[0].user.firstName", is(testUser.getFirstName())))
-            .andExpect(jsonPath("$[0].user.lastName", is(testUser.getLastName())))
-            .andExpect(jsonPath("$[0].user.apiKey", is(testUser.getapiKey())))
-            .andExpect(jsonPath("$[0].user.userAddress.town", is(testUserAddress.getTown())))
-            .andExpect(jsonPath("$[0].user.userAddress.stateOrProvince", is(testUserAddress.getStateOrProvince())))
-            .andExpect(jsonPath("$[0].user.userAddress.postcode", is(testUserAddress.getPostcode())))
-            .andExpect(jsonPath("$[0].user.userAddress.contactTelephone1", is(testUserAddress.getContactTelephone1())))
-            .andExpect(jsonPath("$[0].user.userAddress.contactTelephone2", is(testUserAddress.getContactTelephone2())))
-            .andExpect(jsonPath("$[0].user.userAddress.userAddressLine1", is(testUserAddress.getUserAddressLine1())))
-            .andExpect(jsonPath("$[0].user.userAddress.country.id", is(testCountry.getId())))
-            .andExpect(jsonPath("$[0].user.userAddress.country.title", is(testCountry.getTitle())))
-            .andExpect(jsonPath("$[0].user.userAddress.country.isocode", is(testCountry.getISOCode())))
-            .andExpect(jsonPath("$[0].user.userAddress.country.continentCode", is(testCountry.getContinentCode())));
+            .andExpect(jsonPath("$[0].id", is(testUser.getId())))
+            .andExpect(jsonPath("$[0].username", is(testUser.getUsername())))
+            .andExpect(jsonPath("$[0].password", is(testUser.getPassword())))
+            .andExpect(jsonPath("$[0].emailAddr", is(testUser.getEmailAddr())))
+            .andExpect(jsonPath("$[0].firstName", is(testUser.getFirstName())))
+            .andExpect(jsonPath("$[0].lastName", is(testUser.getLastName())))
+            .andExpect(jsonPath("$[0].apiKey", is(testUser.getapiKey())))
+            .andExpect(jsonPath("$[0].userAddress.town", is(testUserAddress.getTown())))
+            .andExpect(jsonPath("$[0].userAddress.stateOrProvince", is(testUserAddress.getStateOrProvince())))
+            .andExpect(jsonPath("$[0].userAddress.postcode", is(testUserAddress.getPostcode())))
+            .andExpect(jsonPath("$[0].userAddress.contactTelephone1", is(testUserAddress.getContactTelephone1())))
+            .andExpect(jsonPath("$[0].userAddress.contactTelephone2", is(testUserAddress.getContactTelephone2())))
+            .andExpect(jsonPath("$[0].userAddress.userAddressLine1", is(testUserAddress.getUserAddressLine1())))
+            .andExpect(jsonPath("$[0].userAddress.country.id", is(testCountry.getId())))
+            .andExpect(jsonPath("$[0].userAddress.country.title", is(testCountry.getTitle())))
+            .andExpect(jsonPath("$[0].userAddress.country.isocode", is(testCountry.getISOCode())))
+            .andExpect(jsonPath("$[0].userAddress.country.continentCode", is(testCountry.getContinentCode())));
 
-        this.mockmvc.perform(get("/evcharge/api/{apikey}/users","123456888")
-            .contentType(MediaType.APPLICATION_JSON))
+        this.mockmvc.perform(get("/evcharge/api/users","user1")
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("X-OBSERVATORY-AUTH", "1:1:1"))
             .andExpect(status().isUnauthorized());
     }
 
     @Test
-    void testGetOneUsers() throws Exception { 
+    void testGetOneUser() throws Exception { 
 
         MockitoAnnotations.initMocks(this);
         this.mockmvc = webAppContextSetup(this.wac).build();
@@ -136,86 +144,47 @@ class VehicleOwnerTest {
 
 
 
-        BDDMockito.when(repository.findById(1)).thenReturn(Optional.of(testuser));
+        BDDMockito.when(repository2.findById(1)).thenReturn(Optional.of(testUser));
 
-        List<Integer> apiKeys = new ArrayList<>();
-        apiKeys.add(1);
+        List<User> users = new ArrayList();
+        List<Admin> admins = new ArrayList();
+        List<StationOwner> stationOwners = new ArrayList();
+        BDDMockito.when(repository2.findByIdAndApiKey(1,"1")).thenReturn(users);
+        BDDMockito.when(repository4.findByIdAndApiKey(1,"1")).thenReturn(stationOwners);
+        BDDMockito.when(repository5.findByIdAndApiKey(1,"1")).thenReturn(admins);
 
-        BDDMockito.when(repository2.findAdminByApiKey("123456789")).thenReturn(apiKeys);
-
-        this.mockmvc.perform(get("/evcharge/api/{apikey}/users/{id}","123456789",1)
-            .contentType(MediaType.APPLICATION_JSON))
+        this.mockmvc.perform(get("/evcharge/api/users/{id}",1)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("X-OBSERVATORY-AUTH", "wiefweifhbv2397f2vfu22837514899tyjiwbc"))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.user.id", is(testUser.getId())))
-            .andExpect(jsonPath("$.user.username", is(testUser.getUsername())))
-            .andExpect(jsonPath("$.user.password", is(testUser.getPassword())))
-            .andExpect(jsonPath("$.user.emailAddr", is(testUser.getEmailAddr())))
-            .andExpect(jsonPath("$.user.firstName", is(testUser.getFirstName())))
-            .andExpect(jsonPath("$.user.lastName", is(testUser.getLastName())))
-            .andExpect(jsonPath("$.user.apiKey", is(testUser.getapiKey())))
-            .andExpect(jsonPath("$.user.userAddress.town", is(testUserAddress.getTown())))
-            .andExpect(jsonPath("$.user.userAddress.stateOrProvince", is(testUserAddress.getStateOrProvince())))
-            .andExpect(jsonPath("$.user.userAddress.postcode", is(testUserAddress.getPostcode())))
-            .andExpect(jsonPath("$.user.userAddress.contactTelephone1", is(testUserAddress.getContactTelephone1())))
-            .andExpect(jsonPath("$.user.userAddress.contactTelephone2", is(testUserAddress.getContactTelephone2())))
-            .andExpect(jsonPath("$.user.userAddress.userAddressLine1", is(testUserAddress.getUserAddressLine1())))
-            .andExpect(jsonPath("$.user.userAddress.country.id", is(testCountry.getId())))
-            .andExpect(jsonPath("$.user.userAddress.country.title", is(testCountry.getTitle())))
-            .andExpect(jsonPath("$.user.userAddress.country.isocode", is(testCountry.getISOCode())))
-            .andExpect(jsonPath("$.user.userAddress.country.continentCode", is(testCountry.getContinentCode())));
+            .andExpect(jsonPath("$.id", is(testUser.getId())))
+            .andExpect(jsonPath("$.username", is(testUser.getUsername())))
+            .andExpect(jsonPath("$.password", is(testUser.getPassword())))
+            .andExpect(jsonPath("$.emailAddr", is(testUser.getEmailAddr())))
+            .andExpect(jsonPath("$.firstName", is(testUser.getFirstName())))
+            .andExpect(jsonPath("$.lastName", is(testUser.getLastName())))
+            .andExpect(jsonPath("$.apiKey", is(testUser.getapiKey())))
+            .andExpect(jsonPath("$.userAddress.town", is(testUserAddress.getTown())))
+            .andExpect(jsonPath("$.userAddress.stateOrProvince", is(testUserAddress.getStateOrProvince())))
+            .andExpect(jsonPath("$.userAddress.postcode", is(testUserAddress.getPostcode())))
+            .andExpect(jsonPath("$.userAddress.contactTelephone1", is(testUserAddress.getContactTelephone1())))
+            .andExpect(jsonPath("$.userAddress.contactTelephone2", is(testUserAddress.getContactTelephone2())))
+            .andExpect(jsonPath("$.userAddress.userAddressLine1", is(testUserAddress.getUserAddressLine1())))
+            .andExpect(jsonPath("$.userAddress.country.id", is(testCountry.getId())))
+            .andExpect(jsonPath("$.userAddress.country.title", is(testCountry.getTitle())))
+            .andExpect(jsonPath("$.userAddress.country.isocode", is(testCountry.getISOCode())))
+            .andExpect(jsonPath("$.userAddress.country.continentCode", is(testCountry.getContinentCode())));
 
-        this.mockmvc.perform(get("/evcharge/api/{apikey}/users/{id}","123456888",1)
-            .contentType(MediaType.APPLICATION_JSON))
+        this.mockmvc.perform(get("/evcharge/api/users/{id}",1)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("X-OBSERVATORY-AUTH", "1:1:1"))
             .andExpect(status().isUnauthorized());
 
-        this.mockmvc.perform(get("/evcharge/api/{apikey}/users/{id}","123456789",10000)
-        .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isPaymentRequired());
+        this.mockmvc.perform(get("/evcharge/api/users/{id}",10000)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("X-OBSERVATORY-AUTH", "wiefweifhbv2397f2vfu22837514899tyjiwbc"))
+            .andExpect(status().isPaymentRequired());
     }
-    @Test
-    void testGetUsersAdmin() throws Exception { 
 
-        MockitoAnnotations.initMocks(this);
-        this.mockmvc = webAppContextSetup(this.wac).build();
-        Date date = java.sql.Date.valueOf(LocalDate.parse("20201010", DateTimeFormatter.BASIC_ISO_DATE));
-
-        Country testCountry = new Country(1,"mytitle","myiso", "mycont");
-        UserAddress testUserAddress = new UserAddress(1,"test_address_line", "testTown", "TestState", "TestPostcode",
-                        "testPhone1", "testPhone2", testCountry);
-
-        User testUser = new User("user1", "pass1", "email1" , "name1", "name2" , date, testUserAddress,"apikey" );
-
-        BDDMockito.when(repository.findAll()).thenReturn(myList);
-
-        List<Integer> apiKeys = new ArrayList<>();
-        apiKeys.add(1);
-
-        BDDMockito.when(repository2.findAdminByApiKey("123456789")).thenReturn(apiKeys);
-
-        this.mockmvc.perform(get("/evcharge/api/{apikey}/admin/users/{username}","123456789","Kiara41487")
-            .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$[0].user.id", is(testUser.getId())))
-            .andExpect(jsonPath("$[0].user.username", is(testUser.getUsername())))
-            .andExpect(jsonPath("$[0].user.password", is(testUser.getPassword())))
-            .andExpect(jsonPath("$[0].user.emailAddr", is(testUser.getEmailAddr())))
-            .andExpect(jsonPath("$[0].user.firstName", is(testUser.getFirstName())))
-            .andExpect(jsonPath("$[0].user.lastName", is(testUser.getLastName())))
-            .andExpect(jsonPath("$[0].user.apiKey", is(testUser.getapiKey())))
-            .andExpect(jsonPath("$[0].user.userAddress.town", is(testUserAddress.getTown())))
-            .andExpect(jsonPath("$[0].user.userAddress.stateOrProvince", is(testUserAddress.getStateOrProvince())))
-            .andExpect(jsonPath("$[0].user.userAddress.postcode", is(testUserAddress.getPostcode())))
-            .andExpect(jsonPath("$[0].user.userAddress.contactTelephone1", is(testUserAddress.getContactTelephone1())))
-            .andExpect(jsonPath("$[0].user.userAddress.contactTelephone2", is(testUserAddress.getContactTelephone2())))
-            .andExpect(jsonPath("$[0].user.userAddress.userAddressLine1", is(testUserAddress.getUserAddressLine1())))
-            .andExpect(jsonPath("$[0].user.userAddress.country.id", is(testCountry.getId())))
-            .andExpect(jsonPath("$[0].user.userAddress.country.title", is(testCountry.getTitle())))
-            .andExpect(jsonPath("$[0].user.userAddress.country.isocode", is(testCountry.getISOCode())))
-            .andExpect(jsonPath("$[0].user.userAddress.country.continentCode", is(testCountry.getContinentCode())));
-
-        this.mockmvc.perform(get("/evcharge/api/{apikey}/admin/users/{username}","123456888")
-            .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isUnauthorized());
-    }
     
 }
